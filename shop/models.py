c4ca934b6569a5dde35e4acc30be9038b42223f4
sabbir1021 <5boysbd@hmail.com>
2022-08-09 16:09:32 +0600

    tempaltes set and order model create

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from base.models import MEDICINE_TYPE
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 # Create your models here.
 class Generic(models.Model):
     name  = models.CharField(max_length=150)
@@ -28,3 +31,37 @@ class Medicine(models.Model):
     
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    complete_order = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+            verbose_name = 'Orders'
+            verbose_name_plural = '1. Orders'
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        items = self.orderitem_set.all()
+        return sum(i.get_total for i in items)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+
+    class Meta:
+            verbose_name = 'OrderItems'
+            verbose_name_plural = '2. OrderItems'
+
+    def __str__(self):
+        return str(self.order)
+    
+    @property
+    def get_total(self):
+        return (self.product.new_price if self.product.new_price else self.product.price)  * self.quantity
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.views import View 
-
+from .models import Medicine, Brand , Generic
 # Create your views here.
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "shop/home.html")
+        brands = Brand.objects.all()
+        generics = Generic.objects.all()
+        medicines = Medicine.objects.all()
+        context = {
+            'brands':brands,
+            'generics':generics,
+            'medicines':medicines,
+
+        }
+        return render(request, "shop/home.html", context)
