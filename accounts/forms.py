b43e7cb8df7deda91127f5cdb835b615ece2b169
sabbir5564 <sabbir.ahmed@goldkinen.com>
2025-12-15 00:29:12 +0600

    fix all

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 User = get_user_model()
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
